e9454dfc8a83182fc04029838050e6f8e6a17ef6
1PSE <1tjdms0414@naver.com>
2026-07-23 04:15:11 +0900

    feat: 프로젝트 초대 링크 설정 및 응답 모델 추가

--- src/main/java/com/synq/backend/domain/project/code/ProjectErrorCode.java
+++ src/main/java/com/synq/backend/domain/project/code/ProjectErrorCode.java
@@ -8,12 +8,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ProjectErrorCode implements BaseCode {
+	NOT_PROJECT_OWNER(HttpStatus.FORBIDDEN,
+			"PROJECT403_1",
+			"프로젝트 소유자만 초대 링크를 생성할 수 있습니다."),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND,
 			"PROJECT404_1",
 			"사용자를 찾을 수 없습니다."),
 	INVITATION_NOT_FOUND(HttpStatus.NOT_FOUND,
 			"PROJECT404_2",
 			"초대 정보를 찾을 수 없습니다."),
+	PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND,
+			"PROJECT404_3",
+			"프로젝트를 찾을 수 없습니다."),
 	USER_PROJECT_LIMIT_EXCEEDED(HttpStatus.CONFLICT,
 			"PROJECT409_1",
 			"사용자별 최대 프로젝트 수를 초과했습니다."),
